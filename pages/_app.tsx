a654554f3ebac17b71d3d363e577b35de053d5e5
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-17 18:59:12 +0700

    delete: redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.scss";
-import { Provider } from "react-redux";
-import stores from "../store/store";
 import type { AppProps } from "next/app";
-import "bootstrap/dist/css/bootstrap.css";
 import { AuthProvider } from "~/store_context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const store = useStore(pageProps.initialReduxState);
-
   return (
     <AuthProvider>
       <Component {...pageProps} />

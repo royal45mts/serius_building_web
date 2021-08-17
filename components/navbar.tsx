b5a22760d650c7bcbebdf08af28e9ab768432828
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-17 22:13:59 +0700

    fix: scss

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,40 @@
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
+import { useEffect } from "react";
 import styles2 from "~/styles/scss/navbar.module.scss";
 import type { AppProps } from "next/app";
-
 export default function navbar({ children, Test }: any) {
   return (
     <div className={styles2.navbar}>
-      <div></div>
+      <p>
+        <a
+          className="btn btn-primary"
+          data-bs-toggle="collapse"
+          href="#collapseExample"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Link with href
+        </a>
+        <button
+          className="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Button with data-bs-target
+        </button>
+      </p>
+      <div className="collapse" id="collapseExample">
+        <div className="card card-body">
+          Some placeholder content for the collapse component. This panel is
+          hidden by default but revealed when the user activates the relevant
+          trigger.
+        </div>
+      </div>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "~/store_context";
+import { useEffect } from "react";
 // import "bootstrap/dist/js/bootstrap.min.js";
-
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap.bundle.min")
+      : null;
+  }, []);
   return (
     <AuthProvider>
       <Component {...pageProps} />

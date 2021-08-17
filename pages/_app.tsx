0d59dda10f4d99fe00bf0a61cc729e02bfa67952
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-17 17:07:33 +0700

    update: scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import { Provider } from "react-redux";
 import stores from "../store/store";
 import type { AppProps } from "next/app";

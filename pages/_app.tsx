1bd49980efe0883c0f826d53d7f4d95f1accdb8e
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-17 21:50:04 +0700

    add: package.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "~/store_context";
-import "b";
+// import "bootstrap/dist/js/bootstrap.min.js";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>

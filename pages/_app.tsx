15b3b45124865da9cc85d908ab520c9609239efd
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-18 14:24:11 +0700

    update: _app.tsx , README.md

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "~/store_context";
 import { useEffect } from "react";
-// import "bootstrap/dist/js/bootstrap.min.js";
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     typeof document !== undefined

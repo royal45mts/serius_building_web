f3ea53840e6524b4d342526f8a4da01f7bcfc23c
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-30 18:18:08 +0700

    add : boostrap,context api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
+import { AuthProvider } from "~/store_context";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap.bundle.min")
+      : null;
+  }, []);
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
-export default MyApp
+export default MyApp;

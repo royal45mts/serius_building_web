9c10383bc4bbe134dd915e4f94a521836a522159
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-30 19:30:49 +0700

    update : sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,35 @@ const Home = ({ Component, pageProps }: AppProps) => {
     <div>
       <Layout_satu Test={<h5>aws</h5>}>
         <h5 className="text-primary">test</h5>
-        <button className="btn btn-primary">test a</button>
+        <p>
+          <a
+            className="btn btn-primary"
+            data-bs-toggle="collapse"
+            href="#collapseExample"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            Link with href
+          </a>
+          <button
+            className="btn btn-primary"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            Button with data-bs-target
+          </button>
+        </p>
+        <div className="collapse" id="collapseExample">
+          <div className="card card-body">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>{" "}
       </Layout_satu>
     </div>
   );

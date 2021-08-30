436a98ffa93dbb67b1979f23f081a8772cf9fed4
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-08-30 18:14:37 +0700

    add : boostrap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const path = require("path");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    runtimeCaching: [
+      {
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: "/",
+
+        // Apply a cache-first strategy.
+        handler: "CacheFirst",
+
+        options: {
+          // Use a custom cache name.
+          cacheName: "test",
+
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 10,
+          },
+        },
+      },
+    ],
+
+    // sw: "service-worker.js",
+  },
+
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  env: {
+    customKey: ".env",
+  },
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve("./"));
+    return config;
+  },
+});

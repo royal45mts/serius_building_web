80f7c08aaf34f9753ec92b71286881dd75d28a4f
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-05 12:11:22 +0700

    deleted : cookie.ts

--- scripts/cookie.ts
+++
@@ -1,9 +0,0 @@
-export const cookie = {
-  set: (name: string, data: string) => {
-    document.cookie = `${name}=${data};`;
-  },
-  get: (name: string) => {
-    const get_cookie_split_array = document.cookie.split(";");
-    console.log(get_cookie_split_array, "check_data");
-  },
-};

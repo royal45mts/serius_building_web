107dd7ae949bcb46b90a279265c940fcc3b656f1
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-05 22:27:15 +0700

    add : font awesome package.json

--- components/com_navbar.tsx
+++ components/com_navbar.tsx
@@ -9,7 +9,7 @@ export default function Com_navbar({ children, Test }: any) {
           <a className="navbar-brand" href="#">
             SERIUS STORE <i className="fas fa-award" />
           </a>
-          {/*<button
+          <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
@@ -18,55 +18,75 @@ export default function Com_navbar({ children, Test }: any) {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>*/}
+            <i className="navbar-toggler-icon" />
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-5 mb-2 mb-lg-0">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+            <div className={`${Style["menudropdown"]}`}>
+              <div className={`${Style["relative"]}`}>menu</div>
+              <div className={`${Style["absolute"]}`}>menu</div>
+            </div>
 
-            <div className="row col-10 g-0">
+            <div className="row col-9 g-0">
               <div className="col-12">
                 <div className="input-group">
                   <input
-                    type="text"
-                    className={`form-control input-group-sm f
-                    ${Style.inputCostume}`}
+                    type="email"
+                    className={`form-control form-control-sm ${Style.inputCostume}`}
+                    id="exampleFormControlInput1"
                     placeholder="Cari"
-                    aria-describedby="basic-addon1"
                   />
                 </div>
+              </div>
+            </div>
+            <div className="row ms-2 col-2 g-0">
+              <div className="col-2">
+                <div className="input-group">
+                  <input
+                    type="email"
+                    className={`form-control form-control-sm ${Style.inputCostume}`}
+                    id="exampleFormControlInput1"
+                    placeholder="Cari"
+                  />
+                </div>
+              </div>
+              <div className="col-6">
+                <ul className="navbar-nav me-3 mb-2 mb-lg-0">
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Menu
+                    </a>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Another action
+                        </a>
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>

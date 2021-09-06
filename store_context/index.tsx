e5b8050dfb2a72bc96906bff9534db25e19b3d5a
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-06 19:55:11 +0700

    add : add redux toolkit

--- store_context/index.tsx
+++ store_context/index.tsx
@@ -1,9 +1,11 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import counterReducer from "~/store_context/reducers/redux_1";
+import counterReducer2 from "~/store_context/reducers/redux_1";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    counter2: counterReducer2,
   },
 });
 

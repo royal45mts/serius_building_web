80c369d4936b8bf932c509c7df31e650e38cf08d
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-06 17:39:19 +0700

    add : class

--- layout/layout_default.tsx
+++ layout/layout_default.tsx
@@ -1,6 +1,9 @@
 import styles2 from "../styles/scss/test.module.scss";
 import type { AppProps } from "next/app";
 import Com_navbar from "~/components/com_navbar";
+import { useAuth } from "~/store_context";
+import { useEffect, useReducer, useState } from "react";
+
 export default function Layout_default({ children, Test }: any) {
   return (
     <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,43 +10,59 @@ import axios from "axios";
 import { useAuth } from "~/store_context";
 import { useReducer, useState, useEffect } from "react";
 import { useRouter } from "next/router";
+
+class Test {
+  nama: string;
+  constructor(data?: any) {
+    this.nama = "w";
+    console.log(data, "test");
+  }
+  public dispart() {
+    type Datas = {
+      context?: any;
+      context_api?: any;
+    };
+
+    const store: Datas = useAuth();
+    const [state, dispatch]: any = useReducer(
+      store.context["dispatch/redux_1"].actions,
+      store.context["store/redux_1"]
+    );
+    return { state, dispatch };
+  }
+  public set_boolean() {
+    type Bolean2 = [any, any];
+
+    const [bolean, set_bolean]: Bolean2 = useState(false);
+    const data_produk = {
+      name: "produk",
+      data: {
+        identity: "admin@admin.com",
+        password: "12345",
+      },
+    };
+    useEffect(() => {
+      if (bolean === true) {
+        /*axios
+          .post("/api/sundareka", data_produk)
+          .then((r) => {
+            console.log(r, "check_data");
+          })
+          .catch((error) => {
+            console.log(error, "check_data");
+          });*/
+        // Cookies.set("user", "da");
+        // Cookies.remove("user");
+        // console.log(document.cookie);
+        set_bolean(false);
+      }
+    }, [bolean]);
+    return { bolean, set_bolean };
+  }
+}
 const Home = ({ children }: any) => {
-  type Datas = {
-    context?: any;
-    context_api?: any;
-  };
-  const store: Datas = useAuth();
-  const [state, dispatch]: any = useReducer(
-    store.context_api["dispatch/nama"].actions,
-    store.context_api["store/nama"]
-  );
-  type Bolean2 = [any, any];
-
-  const [bolean, set_bolean]: Bolean2 = useState(false);
-  const data_produk = {
-    name: "produk",
-    data: {
-      identity: "admin@admin.com",
-      password: "12345",
-    },
-  };
-  useEffect(() => {
-    if (bolean === true) {
-      /*axios
-        .post("/api/sundareka", data_produk)
-        .then((r) => {
-          console.log(r, "check_data");
-        })
-        .catch((error) => {
-          console.log(error, "check_data");
-        });*/
-      // Cookies.set("user", "da");
-      // Cookies.remove("user");
-      // console.log(document.cookie);
-      set_bolean(false);
-    }
-  }, [bolean]);
-
+  const { state, dispatch } = new Test().dispart();
+  const { bolean, set_bolean } = new Test().set_boolean();
   /*const fetcher = (url: any) =>
     fetch(url, {
       method: "post", // *GET, POST, PUT, DELETE, etc.
@@ -57,11 +73,16 @@ const Home = ({ children }: any) => {
   return (
     <Layout_default>
       <div>
-        {JSON.stringify(bolean)}
         {JSON.stringify(state)}
         <button
           className="btn btn-primary"
-          onClick={() => set_bolean((b: any) => (b = true))}
+          onClick={() => dispatch({ type: "tr" })}
+        >
+          send
+        </button>{" "}
+        <button
+          className="btn btn-primary"
+          onClick={() => dispatch({ type: "tr" })}
         >
           send
         </button>

c7a4416cbb94a9ed4840b95b7fd1b520f06e5d52
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-09 23:18:00 +0700

    add : carousel

--- components/com_image_carousel.tsx
+++ components/com_image_carousel.tsx
@@ -42,26 +42,157 @@ class Menu {
       );
     });
   }
-  public slider() {}
+  public slider() {
+    let slider = document.getElementById("slider"),
+      sliderItems = document.getElementById("slides"),
+      prev = document.getElementById("prev"),
+      next = document.getElementById("next");
+
+    function slide(wrapper: any, items: any, prev: any, next: any) {
+      let posX1 = 0,
+        posX2 = 0,
+        posInitial: any,
+        posFinal,
+        threshold = 100,
+        slides = items.getElementsByClassName("slide"),
+        slidesLength = slides.length,
+        slideSize = items.getElementsByClassName("slide")[0].offsetWidth,
+        firstSlide = slides[0],
+        lastSlide = slides[slidesLength - 1],
+        cloneFirst = firstSlide.cloneNode(true),
+        cloneLast = lastSlide.cloneNode(true),
+        index = 0,
+        allowShift = true;
+
+      // Clone first and last slide
+      items.appendChild(cloneFirst);
+      items.insertBefore(cloneLast, firstSlide);
+      wrapper.classList.add("loaded");
+
+      // Mouse events
+      items.onmousedown = dragStart;
+
+      // Touch events
+      items.addEventListener("touchstart", dragStart);
+      items.addEventListener("touchend", dragEnd);
+      items.addEventListener("touchmove", dragAction);
+
+      // Click events
+      prev.addEventListener("click", function () {
+        shiftSlide(-1);
+      });
+      next.addEventListener("click", function () {
+        shiftSlide(1);
+      });
+
+      // Transition events
+      items.addEventListener("transitionend", checkIndex);
+
+      function dragStart(e: any) {
+        e = e || window.event;
+        e.preventDefault();
+        posInitial = items.offsetLeft;
+
+        if (e.type == "touchstart") {
+          posX1 = e.touches[0].clientX;
+        } else {
+          posX1 = e.clientX;
+          document.onmouseup = dragEnd;
+          document.onmousemove = dragAction;
+        }
+      }
+
+      function dragAction(e: any) {
+        e = e || window.event;
+
+        if (e.type == "touchmove") {
+          posX2 = posX1 - e.touches[0].clientX;
+          posX1 = e.touches[0].clientX;
+        } else {
+          posX2 = posX1 - e.clientX;
+          posX1 = e.clientX;
+        }
+        items.style.left = items.offsetLeft - posX2 + "px";
+      }
+
+      function dragEnd(e: any) {
+        posFinal = items.offsetLeft;
+        if (posFinal - posInitial < -threshold) {
+          shiftSlide(1, "drag");
+        } else if (posFinal - posInitial > threshold) {
+          shiftSlide(-1, "drag");
+        } else {
+          items.style.left = posInitial + "px";
+        }
+
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+
+      function shiftSlide(dir?: any, action?: any) {
+        items.classList.add("shifting");
+
+        if (allowShift) {
+          if (!action) {
+            posInitial = items.offsetLeft;
+          }
+
+          if (dir == 1) {
+            items.style.left = posInitial - slideSize + "px";
+            index++;
+          } else if (dir == -1) {
+            items.style.left = posInitial + slideSize + "px";
+            index--;
+          }
+        }
+
+        allowShift = false;
+      }
+
+      function checkIndex() {
+        items.classList.remove("shifting");
+
+        if (index == -1) {
+          items.style.left = -(slidesLength * slideSize) + "px";
+          index = slidesLength - 1;
+        }
+
+        if (index == slidesLength) {
+          items.style.left = -(1 * slideSize) + "px";
+          index = 0;
+        }
+
+        allowShift = true;
+      }
+    }
+
+    slide(slider, sliderItems, prev, next);
+  }
 }
 
 const menu = new Menu();
 const Com_navbar = ({ children }: any) => {
   let [count, se_count] = useState(menu.angka);
+  const slider = new Menu();
+  useEffect(() => {
+    slider.slider();
+  });
   return (
     <div>
-      <div className={`slider`}>
-        <div className={`${Style.wrapper}`}>
-          <div className={`${Style.slides}`}>
-            <span className={`slide`}>1</span>
-            <span className={`slide`}>2</span>
-            <span className={`slide`}>3</span>
-            <span className={`slide`}>4</span>
-            <span className={`slide`}>5</span>
+      <div id="carousel_global">
+        <div id="slider" className="slider">
+          <div className="wrapper">
+            <div id="slides" className="slides">
+              <span className="slide">Slide 1</span>
+              <span className="slide">Slide 2</span>
+              <span className="slide">Slide 3</span>
+              <span className="slide">Slide 4</span>
+              <span className="slide">Slide 5</span>
+            </div>
           </div>
+          <a id="prev" className="control prev"></a>
+          <a id="next" className="control next"></a>
         </div>
-        <div className={`${Style.control} ${Style.prev}`}>prev</div>
-        <div className={`${Style.control} ${Style.next}`}>next</div>
       </div>
     </div>
   );

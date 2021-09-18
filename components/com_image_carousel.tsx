b59b7535270d5280a9649dfb104af01782dd418a
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-09-18 15:36:40 +0700

    update : slider

--- components/com_image_carousel.tsx
+++ components/com_image_carousel.tsx
@@ -44,21 +44,141 @@ class Menu {
       );
     });
   }
-  public slider() {
+  /*
+  public slider() {var slider = document.getElementById('slider'),
+      sliderItems = document.getElementById('slides'),
+      prev = document.getElementById('prev'),
+      next = document.getElementById('next');
+
+    function slide(wrapper, items, prev, next) {
+      var posX1 = 0,
+          posX2 = 0,
+          posInitial,
+          posFinal,
+          threshold = 100,
+          slides = items.getElementsByClassName('slide'),
+          slidesLength = slides.length,
+          slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+          firstSlide = slides[0],
+          lastSlide = slides[slidesLength - 1],
+          cloneFirst = firstSlide.cloneNode(true),
+          cloneLast = lastSlide.cloneNode(true),
+          index = 0,
+          allowShift = true;
+
+      // Clone first and last slide
+      items.appendChild(cloneFirst);
+      items.insertBefore(cloneLast, firstSlide);
+      wrapper.classList.add('loaded');
+
+      // Mouse events
+      items.onmousedown = dragStart;
+
+      // Touch events
+      items.addEventListener('touchstart', dragStart);
+      items.addEventListener('touchend', dragEnd);
+      items.addEventListener('touchmove', dragAction);
+
+      // Click events
+      prev.addEventListener('click', function () { shiftSlide(-1) });
+      next.addEventListener('click', function () { shiftSlide(1) });
+
+      // Transition events
+      items.addEventListener('transitionend', checkIndex);
+
+      function dragStart (e) {
+        e = e || window.event;
+        e.preventDefault();
+        posInitial = items.offsetLeft;
+
+        if (e.type == 'touchstart') {
+          posX1 = e.touches[0].clientX;
+        } else {
+          posX1 = e.clientX;
+          document.onmouseup = dragEnd;
+          document.onmousemove = dragAction;
+        }
+      }
+
+      function dragAction (e) {
+        e = e || window.event;
+
+        if (e.type == 'touchmove') {
+          posX2 = posX1 - e.touches[0].clientX;
+          posX1 = e.touches[0].clientX;
+        } else {
+          posX2 = posX1 - e.clientX;
+          posX1 = e.clientX;
+        }
+        items.style.left = (items.offsetLeft - posX2) + "px";
+      }
+
+      function dragEnd (e) {
+        posFinal = items.offsetLeft;
+        if (posFinal - posInitial < -threshold) {
+          shiftSlide(1, 'drag');
+        } else if (posFinal - posInitial > threshold) {
+          shiftSlide(-1, 'drag');
+        } else {
+          items.style.left = (posInitial) + "px";
+        }
+
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+
+      function shiftSlide(dir, action) {
+        items.classList.add('shifting');
+
+        if (allowShift) {
+          if (!action) { posInitial = items.offsetLeft; }
+
+          if (dir == 1) {
+            items.style.left = (posInitial - slideSize) + "px";
+            index++;
+          } else if (dir == -1) {
+            items.style.left = (posInitial + slideSize) + "px";
+            index--;
+          }
+        };
+
+        allowShift = false;
+      }
+
+      function checkIndex (){
+        items.classList.remove('shifting');
+
+        if (index == -1) {
+          items.style.left = -(slidesLength * slideSize) + "px";
+          index = slidesLength - 1;
+        }
+
+        if (index == slidesLength) {
+          items.style.left = -(1 * slideSize) + "px";
+          index = 0;
+        }
+
+        allowShift = true;
+      }
+    }
+
+    slide(slider, sliderItems, prev, next);}
+*/
+
+  public slider2() {
     let slider = document.getElementById("slider"),
       sliderItems = document.getElementById("slides"),
       prev = document.getElementById("prev"),
       next = document.getElementById("next");
-
     function slide(wrapper: any, items: any, prev: any, next: any) {
       let posX1 = 0,
         posX2 = 0,
         posInitial: any,
         posFinal,
         threshold = 100,
-        slides = items.getElementsByClassName("slide"),
+        slides = items.getElementsByClassName(Style.slide),
         slidesLength = slides.length,
-        slideSize = items.getElementsByClassName("slide")[0].offsetWidth,
+        slideSize = items.getElementsByClassName(Style.slide)[0].offsetWidth,
         firstSlide = slides[0],
         lastSlide = slides[slidesLength - 1],
         cloneFirst = firstSlide.cloneNode(true),
@@ -131,7 +251,7 @@ class Menu {
         document.onmousemove = null;
       }
 
-      function shiftSlide(dir?: any, action?: any) {
+      function shiftSlide(dir: any, action?: any) {
         items.classList.add("shifting");
 
         if (allowShift) {
@@ -170,24 +290,27 @@ class Menu {
 
     slide(slider, sliderItems, prev, next);
   }
+
   public check_fung() {
     let get_bg = document.getElementById("slides");
-    let get_scroll: any = document.getElementById(Style.scroll_check);
+    let get_scroll: any = get_bg!.getElementsByClassName(Style.slider)[0];
     const check = (event: any) => {
       event.preventDefault();
       // get_scroll.style;
-      // get_scroll.style.transform = "translateX(-30)";
-      console.log(get_bg?.offsetLeft);
+      get_bg!.style.transform = `translateX(${
+        event.clientX - event.clientX + get_bg!.offsetLeft
+      }px)`;
+      console.log(event.clientX);
     };
 
-    get_bg?.addEventListener("mousedown", (e: any) => {
-      get_bg?.addEventListener("mousemove", check);
+    get_bg!.addEventListener("mousedown", (e: any) => {
+      get_bg!.addEventListener("mousemove", check);
     });
-    get_bg?.addEventListener("mouseup", (e: any) => {
-      get_bg?.removeEventListener("mousemove", check);
+    get_bg!.addEventListener("mouseup", (e: any) => {
+      get_bg!.removeEventListener("mousemove", check);
     });
-    get_bg?.addEventListener("mouseout", (e: any) => {
-      get_bg?.removeEventListener("mousemove", check);
+    get_bg!.addEventListener("mouseout", (e: any) => {
+      get_bg!.removeEventListener("mousemove", check);
     });
   }
 }
@@ -195,10 +318,12 @@ let array: any = [];
 for (let i = 0; i < 5; i++) {
   array.push(i);
 }
-const menu = new Menu();
+
 const Com_navbar = ({ children }: any) => {
+  const menu = new Menu();
+
   useEffect(() => {
-    menu.check_fung();
+    menu.slider2();
   }, []);
   return (
     <div>
@@ -219,11 +344,20 @@ const Com_navbar = ({ children }: any) => {
         </div>
       </div>
 */}
-      <div id="slider" className={`${Style.scroll_check}`}>
-        <div id="slides" className={`${Style.background}`}>
-          {array.map((d: any, i: number) => {
-            return <div key={i}>test</div>;
-          })}
+
+      <div id="slider" className={`${Style.scroll_check} slider`}>
+        <div className="wrapper">
+          <div id="slides" className={`${Style.background} slides`}>
+            {array.map((d: any, i: number) => {
+              return (
+                <div className={Style.slide} key={i}>
+                  test
+                </div>
+              );
+            })}
+            <a id="prev" className="control prev"></a>
+            <a id="next" className="control next"></a>
+          </div>
         </div>
       </div>
     </div>

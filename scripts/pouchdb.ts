a4948299da42933f474df43176a399801e2f908f
Aldi Iskandar <aldiiskandar4@gmail.com>
2021-10-09 11:54:58 +0700

    add : couchbase

--- scripts/pouchdb.ts
+++ scripts/pouchdb.ts
@@ -3,7 +3,12 @@ Pouchdb.plugin(require("pouchdb-find"));
 
 const pouchdb = (local: string) => {
   const db = new Pouchdb(local);
-  const remote = new Pouchdb("http://localhost:8092/");
+  const remote = new Pouchdb("http://localhost:8092/", {
+    fetch(url: any, opts: any) {
+      opts.headers.set("Content-Type", "application/json");
+      return Pouchdb.fetch(url, opts);
+    },
+  });
 
   return {
     db: db,

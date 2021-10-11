afef73f73970f397fabd89a79b269efa092e3f83
Aldi Iskandar <aldiiskandar4@gmail.com>
2021-10-11 21:24:11 +0700

    delete : revs_limit

--- scripts/pouchdb.ts
+++ scripts/pouchdb.ts
@@ -3,10 +3,13 @@ Pouchdb.plugin(require("pouchdb-find"));
 Pouchdb.plugin(require("pouchdb-authentication"));
 
 const pouchdb = (name: string, local: string) => {
-  const db = new Pouchdb(local);
+  const db = new Pouchdb(local, {
+    revs_limit: 1,
+  });
   const auth =
     "Basic " + Buffer.from("root" + ":" + "12345678").toString("base64");
   const remote = new Pouchdb(`http://localhost:5984/${name}_${local}`, {
+    revs_limit: 1,
     fetch(url: any, opts: any) {
       opts.headers.set("Authorization", auth);
       opts.headers.set("Content-Type", "application/json");
@@ -16,7 +19,7 @@ const pouchdb = (name: string, local: string) => {
   });
   return {
     db: db,
-    // remote: remote,
+    remote: remote,
   };
 };
 
